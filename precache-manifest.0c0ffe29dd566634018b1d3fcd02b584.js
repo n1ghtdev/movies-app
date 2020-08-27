7b89a8905d7508fe7c6a4c3ea204aa717d58a593
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-27 12:30:54 +0000

    Deploy n1ghtdev/movies-app to github.com/n1ghtdev/movies-app.git:gh-pages

---
+++ precache-manifest.0c0ffe29dd566634018b1d3fcd02b584.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93690d06787115bdb8cde9b351562cf0",
+    "url": "/movies-app/index.html"
+  },
+  {
+    "revision": "f6469980f24b1028a415",
+    "url": "/movies-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6c12794f7c612e23c203",
+    "url": "/movies-app/static/js/2.956e9d31.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/movies-app/static/js/2.956e9d31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6469980f24b1028a415",
+    "url": "/movies-app/static/js/main.ad4422a8.chunk.js"
+  },
+  {
+    "revision": "6f4ce7a2b201b6f9f6f8",
+    "url": "/movies-app/static/js/runtime-main.94bcfe81.js"
+  }
+]);
